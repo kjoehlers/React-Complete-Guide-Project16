0c2efb2be2efd31fe49457f3db87d6e8efab5d88
Ken Oehlers <kjojr56@gmail.com>
2023-03-19 11:06:19 -0400

    Commit before Lesson 206

---
+++ src/hooks/use-input.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const useInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  // input handler
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+  // blur handler
+  const inputBlurHandler = (event) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    isValid: valueIsValid,
+    hasError: hasError,
+    valueChangeHandler: valueChangeHandler,
+    inputBlurHandler: inputBlurHandler,
+    reset: reset,
+  };
+};
+
+export default useInput;
